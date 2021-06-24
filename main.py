4a45a74c383b1589e7e13cbc9fd681f6560cb8d0
thamugadi <22577625+thamugadi@users.noreply.github.com>
2021-06-24 15:05:02 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import numpy as np
+import matplotlib.pyplot as plt
 def sigmoid(x):
     return 1/(1+np.exp(-x))
 def dsigmoid(x):
     return (sigmoid(x))*(1-sigmoid(x))
 def dMSE(A,B):
     return 2*(A-B)
-
+def MSE(A,B):
+    return ((A-B)**2).mean()
 class VanillaLayer:
     def __init__(self, inputLength,outputLength, activation,derivative):
         self.inputLength = inputLength
@@ -49,7 +51,7 @@ class VanillaNetwork:
         self.houtput = h
         self.output = output
         return output
-    def backpropagate(self, input, correct, dCost):
+    def backpropagate(self, input, correct, dCost, cost):
         self.evaluate(np.array(input))
         self.gradients = []
         n = len(self.layers)
@@ -58,6 +60,8 @@ class VanillaNetwork:
             layer.computeError(nextlayer)
         for layer in self.layers:
             self.gradients.append(layer.error)
+        self.cost = cost(self.output, correct)
+
     def adjustWeights(self, rate):
         for (layer, a) in zip(self.layers, self.network):
             layer.wmatrix = layer.wmatrix - rate*a*layer.error
@@ -65,12 +69,21 @@ class VanillaNetwork:
         for layer in self.layers:
             layer.bvector = layer.bvector - rate*layer.error
 
+    def train(self,datasetX, datesetC, dCost, cost, rate, epochs):
+        for _ in epochs:
+            for x,c in zip(datasetX, datasetC):
+                self.backpropagate(x,c,dCost,cost)
+                self.adjustWeights(rate)
+                self.adjustBiases(rate)
+
 H1 = HiddenLayer(3,3,sigmoid,dsigmoid)
 O1 = OutputLayer(3,3,sigmoid,dsigmoid)
 
 N = VanillaNetwork()
 N.add(H1)
 N.add(O1)
-for _ in range(1001):
-    N.backpropagate([0,0,1],[1,0,0],dMSE)
-    N.adjustWeights(0.2)
+X = []
+Y = []
+
+plt.plot(X,Y)
+plt.show()
